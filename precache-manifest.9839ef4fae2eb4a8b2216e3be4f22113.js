829e9ac92326d387069a4b1c758e78ad5e8fa758
Hongtao Huang <hhuang@godaddy.com>
2019-08-06 13:09:36 -0700

    Updates

---
+++ precache-manifest.9839ef4fae2eb4a8b2216e3be4f22113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1808e763701888e9f25550112895098b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "179e8057671594431839",
+    "url": "/static/css/main.e3f4fb5b.chunk.css"
+  },
+  {
+    "revision": "ebc815c63c6190892d26",
+    "url": "/static/js/2.173a05f2.chunk.js"
+  },
+  {
+    "revision": "179e8057671594431839",
+    "url": "/static/js/main.bf3d5c7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GoogleDriveProxy/precache-manifest.973067adeb224bf627f04651dc07bff2.js"
+  "/precache-manifest.9839ef4fae2eb4a8b2216e3be4f22113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GoogleDriveProxy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
